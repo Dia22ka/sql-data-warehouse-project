a3cdadc0e9523b0e9aa87e3d2e0ba68e5795dfc0
Dia22ka <diana.karpusss@gmail.com>
2025-12-28 19:19:25 +0100

    PostgreSQL

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -13,30 +13,69 @@
     and ensure you have proper backups before running the script.
  */
 
-USE master;
-GO
+--Create Database 'DataWarehouse'
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-DROP DATABASE DataWarehouse;
-END;
-GO
-
--- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-GO
 
-USE DataWarehouse;
-
--- Create schemas
 CREATE SCHEMA bronze;
-GO
-
 CREATE SCHEMA silver;
-GO
-
 CREATE SCHEMA gold;
-GO
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info(
+    cst_id              INT,
+    cst_key             TEXT,
+    cst_firstname       TEXT,
+    cst_lastname        TEXT,
+    cst_marital_status  TEXT,
+    cst_gndr            TEXT,
+    cst_create_date     DATE
+);
+
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info(
+    prd_id          INT,
+    prd_key         TEXT,
+    prd_nm          TEXT,
+    prd_cost        INT,
+    prd_line        CHAR(1),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE
+);
+
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details(
+    sls_ord_num     TEXT,
+    sls_prd_key     TEXT,
+    sls_cust_id     INT,
+    sls_order_dt    INT,
+    sls_ship_dt     INT,
+    sls_due_dt      INT,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT
+);
+
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101(
+    cid     TEXT,
+    cntry   TEXT
+);
+
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12(
+    cid     TEXT,
+    bdate   DATE,
+    gen     TEXT
+);
+
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1;
+CREATE TABLE bronze.erp_px_cat_g1v2(
+    id          TEXT,
+    cat         TEXT,
+    subcat      TEXT,
+    maintenance TEXT
+);
+
+
 
